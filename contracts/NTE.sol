4b5da2528f373d96ac21ab0c6dff3135653db0af
Node Meta <nodemetabusiness@gmail.com>
2026-02-22 11:27:54 +0600

    F-2026-15018 revert

--- contracts/NTE.sol
+++ contracts/NTE.sol
@@ -2033,7 +2033,7 @@ contract NTE is IERC20 {
         ) {
             _transfer(from, to, amount);
         } else {
-            uint256 tax = _calculateTax(from, to, amount, msg.sender);
+            uint256 tax = _calculateTax(from, to, amount);
             
             if (tax > 0) {
                 if (treasury == address(0)) revert TAX_TREASURY_ZERO();
@@ -2074,34 +2074,25 @@ contract NTE is IERC20 {
     /**
      * @dev Deciding which tax rate applies to this specific trade.
      *      We check if it's a Buy, a Sell, or just a friend-to-friend (P2P) move.
-     *      Liquidity operations orchestrated by the router are excluded from buy/sell taxes.
      * @param from Sender address.
      * @param to Recipient address.
      * @param amount Base amount.
-     * @param initiator The address initiating the transfer (msg.sender).
      * @return The amount of tokens to take as tax.
      */
-    function _calculateTax(address from, address to, uint256 amount, address initiator) private view returns (uint256) {
+    function _calculateTax(address from, address to, uint256 amount) private view returns (uint256) {
         uint256 taxBps = 0;
         
-        // If router is the initiator, it's a liquidity operation (add/remove), not a swap
-        bool isLiquidityOp = (initiator == pancakeRouter);
-        
         if (isPancakePair[from] && !isPancakePair[to]) {
-            // Buy: from DEX to user (but not during router-orchestrated liquidity removal)
-            if (!isLiquidityOp) {
-                taxBps = buyTaxBps;
-            }
+            // Buy: from DEX to user
+            taxBps = buyTaxBps;
         } else if (!isPancakePair[from] && isPancakePair[to]) {
-            // Sell: from user to DEX (but not during router-orchestrated liquidity addition)
-            if (!isLiquidityOp) {
-                taxBps = sellTaxBps;
-            }
+            // Sell: from user to DEX
+            taxBps = sellTaxBps;
         } else if (isPancakePair[from] && isPancakePair[to]) {
             // Pool-to-pool move (usually arbitrage)
             taxBps = sellTaxBps;
         } else if (pancakeRouter != from && pancakeRouter != to) {
-            // Just a regular P2P transfer (exclude router to prevent double taxation on liquidity removal)
+            // Regular P2P transfer (exclude router to prevent double taxation)
             taxBps = transferTaxBps;
         }
         
